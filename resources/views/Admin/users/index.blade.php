e33812a1d96d98da7fa9b015df52f9c0a7e65564
MinhQuang <minhquanga32001@gmail.com>
2023-05-16 18:52:11 +0700

    update

--- resources/views/Admin/users/index.blade.php
+++ resources/views/Admin/users/index.blade.php
@@ -3,35 +3,35 @@
 
 
 @section('content')
-   <div class="card">
-    <div class="card-header">
-        <h4>Register Users</h4>
+    <div class="card">
+        <div class="card-header">
+            <h4>Danh sách người dùng</h4>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped table-fixed table-hover align-middle text-center">
+                <thead class="table-dark">
+                    <tr>
+                        <th>Id</th>
+                        <th>Tên</th>
+                        <th>Email</th>
+                        <th>Số điện thoại</th>
+                        <th>Hành động</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $item)
+                        <tr>
+                            <td>{{ $item->id }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->email }}</td>
+                            <td>{{ $item->phoneno }}</td>
+                            <td>
+                                <a class="btn btn-primary" href="{{ url('view-user/' . $item->id) }}">View</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-    <div class="card-body">
-        <table class="table table-striped table-fixed table-hover align-middle text-center">
-            <thead class="table-dark">
-                <tr>
-                    <th>Id</th>
-                    <th >Name</th>
-                    <th >Email</th>
-                    <th>Phone No</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $item)    
-                <tr>
-                    <td >{{$item->id}}</td>
-                    <td >{{$item->name}}</td>
-                    <td >{{$item->email}}</td>
-                    <td >{{$item->phoneno}}</td>
-                    <td>
-                        <a class="btn btn-primary" href="{{url('view-user/'.$item->id)}}">View</a>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-   </div>
 @endsection
